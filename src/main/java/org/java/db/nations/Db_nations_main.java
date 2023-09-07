8ec4d29109898b152b4c5fad232341fee81d690f
Oblv49 <noprivacyworld@gmail.com>
2023-09-07 14:26:17 +0200

    milestone 3

--- src/main/java/org/java/db/nations/Db_nations_main.java
+++ src/main/java/org/java/db/nations/Db_nations_main.java
@@ -1,24 +1,44 @@
 package org.java.db.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Db_nations_main {
     public static void main(String[] args) {
-        System.out.println("Nazioni: ");
+        System.out.println("Inserisci la stringa di ricerca: ");
+        Scanner scanner = new Scanner(System.in);
+        String searchString = scanner.nextLine();
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nation", "root", "Nuova_password1!!");
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery("SELECT countries.name AS Lista_Delle_Nazioni, countries.country_id AS ID_Nazione, " +
-                                                           "regions.name AS Nome_Regione, continents.name AS Nome_Continente FROM countries " +
-                                                           "JOIN regions ON countries.region_id = regions.region_id " +
-                                                           "JOIN continents ON regions.continent_id = continents.continent_id " +
-                                                           "ORDER BY countries.name");
-            while (results.next()) {
-                System.out.println(results.getString("Lista_Delle_Nazioni") + ", "
-                        + results.getString("ID_Nazione") + ", "
-                        + results.getString("Nome_Regione") + ", "
-                        + results.getString("Nome_Continente"));
+            String stringaConnessione = "jdbc:mysql://localhost:3306/nation";
+            String username = "root";
+            String password = "Nuova_password1!!";
+            Connection connessione = DriverManager.getConnection(stringaConnessione, username, password);
+            if (connessione == null) {
+                throw new SQLException("Connessione al database non riuscita");
             }
+            String query = "SELECT countries.name AS Lista_Delle_Nazioni, countries.country_id AS ID_Nazione, " +
+                            "regions.name AS Nome_Regione, continents.name AS Nome_Continente FROM countries " +
+                            "JOIN regions ON countries.region_id = regions.region_id " +
+                            "JOIN continents ON regions.continent_id = continents.continent_id " +
+                            "WHERE countries.name LIKE ? " +
+                            "ORDER BY countries.name";
+
+            PreparedStatement statement = connessione.prepareStatement(query);
+            statement.setString(1, "%" + searchString + "%");
+
+            ResultSet risultati = statement.executeQuery();
+            boolean found = false;
+            while (risultati.next()) {
+                System.out.println(risultati.getString("Lista_Delle_Nazioni") + ", "
+                        + risultati.getString("ID_Nazione") + ", "
+                        + risultati.getString("Nome_Regione") + ", "
+                        + risultati.getString("Nome_Continente"));
+                found = true;
+            }
+            if (!found) {
+                System.out.println("Nessun risultato trovato.");
+            }
+            connessione.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
